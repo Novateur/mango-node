b46aecc7374278bcb41b255996b9583e6bcb8c91
David Kezi <davidkezi6@gmail.com>
2017-06-12 17:28:40 +0100

    receiving message feature added

--- routes/index.js
+++ routes/index.js
@@ -2,29 +2,35 @@ var express = require('express');
 var router = express.Router();
 var app = express();
 var http = require('http');
+var bodyParser = require('body-parser');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Nova Voice' });
 });
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // Twilio Credentials
-var accountSid = 'ACe03a60785b546e11a9034f0d05aec209';
-var authToken = '582a7dd229394e8310bdce02ce279451';
+// Mr vals details
+var accountSid = 'ACd1075aeb55a161ac7e1e5c28468cbf18';
+var authToken = '3fb92f0007c07c1d3ced68f7ae8dbe9e';
 
 //require the Twilio module and create a REST client
 var client = require('twilio')(accountSid, authToken);
 
 // client.messages.create({
-// 	to: "+2348189427825",
-// 	from: "+14157921566 ",
-// 	body: "NovaVoice here"
+// 	to: "+23480922",
+// 	from: "+16692312315",
+// 	body: "david is here ahsdbkjah"
 // }, function(err, message) {
 // 	if (err) console.log(err.message);
 // 	if (message) console.log(message.sid);
 // });
 
+
+
 app.post('/sms', function(req, res) {
-	console.log(req.body);
+	console.log(req.body.Body);
 	var msgFrom = req.body.From;
 	var msgBody = req.body.Body;
 
@@ -33,13 +39,18 @@ app.post('/sms', function(req, res) {
 	// twiml.message('received!');
 	// res.writeHead(200, {'Content-Type': 'text/xml'});
 	// res.end(twiml.toString());
-        res.send(`
+
+	res.send(`
 	<Response>
         	<Message>
         		Hello ${msgFrom} this is ${msgBody}
 		</Message>
 	</Response>
-        `)
+	`);
+	// console.log(msgBody);
+
+
+
 });
 
 http.createServer(app).listen(1337, function () {
